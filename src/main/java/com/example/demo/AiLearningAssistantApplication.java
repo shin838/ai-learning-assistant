54ea9815ed808adf4aa9fce25392464a988f9ac5
shin838 <21stseptemberr@gmail.com>
2026-07-14 00:02:30 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/demo/AiLearningAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLearningAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLearningAssistantApplication.class, args);
+	}
+
+}
